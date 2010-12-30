4e98f417ff6032f525340e938b7d8447c4338454
Mamadou Ngor DIOUF <mngdiouf@gmail.com>
2010-12-30 23:38:18 +0000

    Interface CentreConference

---
+++ jcertif-dao/src/main/java/com/jcertif/dao/CentreConferenceDAO.java
@@ -0,0 +1,12 @@
+package com.jcertif.dao;
+
+import com.jcertif.bo.CentreConference;
+import com.jcertif.dao.GenericDAO;
+
+/**
+ * 
+ * @author Mamadou
+ *
+ */
+public interface CentreConferenceDAO extends GenericDAO<CentreConference, Long> {
+}
