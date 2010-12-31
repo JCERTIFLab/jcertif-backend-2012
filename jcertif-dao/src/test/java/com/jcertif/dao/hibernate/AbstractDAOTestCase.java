f774a5e83992c566a2f6c15b81067a261cfc548b
Rossi Oddet <rossi.oddet@gmail.com>
2010-12-31 07:50:24 +0000

    [DAO] Ajout d'une procédure de nettoyage de base de données entre chaque test pour éléminer les adhérences entre les différentes exécutions. Réparation du Build Unstable Hudson. Un fichier clean-dataset.xml a été créé. Il convient d'ajouter toutes les tables dans ce fichier pour s'assurer d'un clean total.

--- jcertif-dao/src/test/java/com/jcertif/dao/hibernate/AbstractDAOTestCase.java
+++ jcertif-dao/src/test/java/com/jcertif/dao/hibernate/AbstractDAOTestCase.java
@@ -3,12 +3,15 @@ package com.jcertif.dao.hibernate;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
 import org.apache.commons.lang.StringUtils;
 import org.dbunit.DataSourceDatabaseTester;
+import org.dbunit.DatabaseUnitException;
 import org.dbunit.IDatabaseTester;
+import org.dbunit.database.DatabaseConnection;
 import org.dbunit.dataset.DataSetException;
 import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
@@ -23,30 +26,49 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.ResourceUtils;
 
-
 @Transactional
 @RunWith(SpringJUnit4ClassRunner.class)
 public abstract class AbstractDAOTestCase {
 
-	
+	/**
+	 * Fichier de nettoyage de la base de données.
+	 */
+	private static final String CLEAN_FILE = "com/jcertif/dao/hibernate/clean-dataset.xml";
+
+	/**
+	 * Un Logger.
+	 */
 	private static final Logger LOGGER = LoggerFactory
 			.getLogger(AbstractDAOTestCase.class);
 
 	/** Sufix for DBUnit files */
 	private static final String DATASET_FILENAME_SUFFIX = "-dataset.xml";
 
+	/**
+	 * Session Factory Hibernate.
+	 */
 	@Autowired
 	private SessionFactory sessionFactory;
 
+	/**
+	 * Datasource utilisé pour les tests.
+	 */
 	@Autowired
 	private DataSource dataSource;
 
-	
+	/**
+	 * Opération effectuée avant chaque test, rechargement du jeux de données.
+	 * 
+	 * @throws Exception
+	 */
 	@Before
 	public void setUpTestDataWithinTransaction() throws Exception {
 
 		final IDatabaseTester databaseTester = new DataSourceDatabaseTester(
 				this.dataSource);
+
+		// Nettoyage de la base de données.
+		cleanDB();
 
 		final IDataSet dataSet = loadDataSet();
 
@@ -62,6 +84,26 @@ public abstract class AbstractDAOTestCase {
 		}
 	}
 
+	/**
+	 * Nettoyage de la base de données.
+	 * 
+	 * @throws DataSetException
+	 * @throws DatabaseUnitException
+	 * @throws SQLException
+	 * @throws IOException
+	 */
+	private void cleanDB() throws DataSetException, DatabaseUnitException,
+			SQLException, IOException {
+		final File file = ResourceUtils
+				.getFile(ResourceUtils.CLASSPATH_URL_PREFIX + CLEAN_FILE);
+		DatabaseOperation.CLEAN_INSERT.execute(new DatabaseConnection(
+				this.dataSource.getConnection()), getXmlDataSet(file));
+	}
+
+	/**
+	 * @return un dataset pour test unitaire.
+	 * @throws Exception
+	 */
 	protected IDataSet loadDataSet() throws Exception {
 		IDataSet dataSet = null;
 
@@ -82,35 +124,28 @@ public abstract class AbstractDAOTestCase {
 		return dataSet;
 	}
 
-	
 	protected IDataSet getXmlDataSet(File file) throws DataSetException,
 			IOException {
 		return new FlatXmlDataSetBuilder().build(file);
 	}
 
-	
 	protected String getDataSetFilename() {
 		return StringUtils.replaceChars(this.getClass().getCanonicalName(),
-				'.', File.separatorChar)
-				+ DATASET_FILENAME_SUFFIX;
+				'.', File.separatorChar) + DATASET_FILENAME_SUFFIX;
 	}
 
-	
 	protected DatabaseOperation getDatabaseSetupOperation() {
 		return DatabaseOperation.CLEAN_INSERT;
 	}
 
-	
 	protected DatabaseOperation getDatabaseTearDownOperation() {
 		return DatabaseOperation.NONE;
 	}
 
-	
 	protected void flush() {
 		sessionFactory.getCurrentSession().flush();
 	}
 
-	
 	protected void clear() {
 		sessionFactory.getCurrentSession().clear();
 	}
@@ -120,17 +155,14 @@ public abstract class AbstractDAOTestCase {
 		clear();
 	}
 
-	
 	public void setSessionFactory(final SessionFactory sessionFactory) {
 		this.sessionFactory = sessionFactory;
 	}
 
-	
 	protected DataSource getDataSource() {
 		return dataSource;
 	}
 
-	
 	public void setDataSource(DataSource dataSource) {
 		this.dataSource = dataSource;
 	}
