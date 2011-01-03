9c2a233f190179144cf0b81707602372e68276ac
Rossi Oddet <rossi.oddet@gmail.com>
2011-01-03 07:42:00 +0000

    [BO] Ajout du mapping minimum pour un build stable au BO CeduleParticipant (en cours de build)

--- jcertif-bo/src/main/java/com/jcertif/bo/CeduleParticipant.java
+++ jcertif-bo/src/main/java/com/jcertif/bo/CeduleParticipant.java
@@ -4,9 +4,16 @@
  */
 package com.jcertif.bo;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 /**
  * @author chrisbel
  */
+@Entity
 public class CeduleParticipant {
+
+@Id
+private Long id;
 
 }
