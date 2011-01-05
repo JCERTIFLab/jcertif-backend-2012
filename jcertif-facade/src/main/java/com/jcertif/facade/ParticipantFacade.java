0e2c9f398e9e7adcb7fc267591e7161f2cf7fa48
Bernard Adanlessossi <adanlessossi@gmail.com>
2011-01-05 23:27:43 +0000

    Added Participant facade and XML utils

---
+++ jcertif-facade/src/main/java/com/jcertif/facade/ParticipantFacade.java
@@ -0,0 +1,129 @@
+/**
+ * 
+ */
+package com.jcertif.facade;
+
+import java.util.Iterator;
+import java.util.List;
+
+import com.jcertif.bo.Participant;
+import com.jcertif.dao.ParticipantDAO;
+import com.jcertif.facade.exception.ItemAlreadyExistsException;
+import com.jcertif.facade.exception.ItemNotFoundException;
+import com.jcertif.ws.participant.ParticipantService;
+import com.jcertif.util.XMLUtil;
+import com.sun.jersey.api.core.InjectParam;
+
+/**
+ * Facade for the {@link ParticipantDAO}.
+ * 
+ * @author Bernard Adanlessossi
+ *
+ */
+public class ParticipantFacade {
+	
+	@InjectParam
+	private static ParticipantService service;
+	
+	public static String create(String xml) throws ItemAlreadyExistsException {
+		Participant participant = XMLUtil.getParticipantVOFromXML(xml);
+        if (participant != null) {
+        	// Check if participant exists in our DB: if not, create; else, throw
+        	// exception
+        	if (service.findById(participant.getId()) == null){
+        		service.save(participant);
+        	}
+
+        		return participant.toXML();
+        	} else {
+        		throw new ItemAlreadyExistsException();
+        	}
+	}
+
+	/**
+	 * Gets all participant as an xml file.
+	 * <p>Note: this method should be moved to the DAO
+	 * @return the xml
+	 */
+	public static String getAllXML() {
+		// Retrieve the participants list
+		List<Participant> result = service.findAll();
+		StringBuilder participants = new StringBuilder();
+		participants.append("<participants>");
+		participants.append("<count>").append(result.size()).append("</count>");
+		while (result.iterator().hasNext()) {
+			participants.append(result.iterator().next().toXML());
+		}
+		participants.append("</participants>");
+		return participants.toString();
+	}
+	
+	public static String getAllJSON() {
+		StringBuilder participants = new StringBuilder();
+		
+		List<Participant> result = service.findAll();
+
+		participants.append("{\"participants-result\":{\"count\":\"").append(result.size()).append("\", \"participants\":[");
+		
+		Iterator<Participant> iter = result.iterator();
+		while (iter.hasNext()) {
+			participants.append(iter.next().toJSON());
+
+			if (iter.hasNext()) {
+				participants.append(",");
+			}
+		}
+
+		participants.append("]}}");
+
+		return participants.toString();
+	}
+	
+	public static String update(String xml) throws ItemNotFoundException {
+		Participant tmpParticipant = null;
+		
+		Participant participant = XMLUtil.getParticipantVOFromXML(xml);
+		if (participant != null) {
+			// Check that user exists in our DB: if so, update; else, throw
+			// exception
+			tmpParticipant = query(participant.getId());
+			
+			if (tmpParticipant != null) {
+				// We only let the user update the email
+				tmpParticipant.setEmail(participant.getEmail());
+				service.update(tmpParticipant);
+				
+			} else {
+				throw new ItemNotFoundException();
+			}
+		}
+		return tmpParticipant.toXML();
+	}
+	
+	public static void delete(Long id) throws ItemNotFoundException {
+		
+		Participant participant = query(id);
+		
+		if (participant != null){
+			service.remove(participant);
+		}else{
+			throw new ItemNotFoundException();
+		}
+	}
+	
+	public static String getXML(Long key) {
+		Participant participant = query(key);
+
+		return (participant != null) ? participant.toXML() : null;
+	}
+	
+	public static String getJSON(Long key) {
+		Participant participant = query(key);
+
+		return (participant != null) ? participant.toJSON() : null;
+	}
+	
+	private static Participant query (Long key){
+		return service.findById(key);		
+	}
+}
---
+++ jcertif-facade/src/main/java/com/jcertif/util/XMLUtil.java
@@ -0,0 +1,173 @@
+/**
+ * 
+ */
+package com.jcertif.util;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+import com.jcertif.bo.Participant;
+
+/**
+ * Utility class for xml files.
+ * 
+ * @author Bernard Adanlessossi
+ *
+ */
+public class XMLUtil {
+
+	public XMLUtil(){
+		super();
+	}
+	
+	private static Document getDocument(String xml) {
+		try {
+			// Create a builder factory
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			factory.setNamespaceAware(true);
+			
+			return factory.newDocumentBuilder().parse(new InputSource(new StringReader(xml)));
+		} catch (SAXException e) {
+			return null;
+		} catch (ParserConfigurationException e) {
+			return null;
+		} catch (IOException e) {
+			return null;
+		}
+	}
+
+	private static boolean validateParticipantXML(Document doc) {
+		NodeList nodeList = null;
+		
+		// Check the elements and values exist
+		nodeList = doc.getElementsByTagName("participant");
+		if (nodeList.getLength() != 1) {
+			return false;
+		}
+
+		// Check that id element exists
+		nodeList = doc.getElementsByTagName("id");
+		if (nodeList.getLength() != 1) {
+			return false;
+		}
+		
+		// Check that dateinscription element exists
+		nodeList = doc.getElementsByTagName("dateinscription");
+		if (nodeList.getLength() != 1) {
+			return false;
+		}
+		
+		// Check that salutation element exists
+		nodeList = doc.getElementsByTagName("salutation");
+		if (nodeList.getLength() != 1) {
+			return false;
+		}
+		
+		// Check that nom element exists
+		nodeList = doc.getElementsByTagName("nom");
+		if (nodeList.getLength() != 1) {
+			return false;
+		}
+		
+		// Check that sexe element exists
+		nodeList = doc.getElementsByTagName("sexe");
+		if (nodeList.getLength() != 1) {
+			return false;
+		}
+		
+		// Check that email element exists
+		nodeList = doc.getElementsByTagName("email");
+		if (nodeList.getLength() != 1) {
+			return false;
+		}
+		
+		// Check that presentationsoumise element exists
+		nodeList = doc.getElementsByTagName("presentationsoumise");
+		if (nodeList.getLength() != 1) {
+			return false;
+		}
+		
+		// Check that cvsoumis element exists
+		nodeList = doc.getElementsByTagName("cvsoumis");
+		if (nodeList.getLength() != 1) {
+			return false;
+		}
+		
+		// Check that details element exists
+		nodeList = doc.getElementsByTagName("details");
+		if (nodeList.getLength() != 1) {
+			return false;
+		}
+		
+		// Check that email element exists
+		nodeList = doc.getElementsByTagName("roleparticipant");
+		if (nodeList.getLength() != 1) {
+			return false;
+		}
+		
+		// Check that value is not null or empty
+		String username = getValue((Element) doc.getElementsByTagName("roleparticipants").item(0), "codeRole");
+		if (username == null || username.isEmpty()) {
+			return false;
+		}
+
+		// Check that email element exists
+		nodeList = doc.getElementsByTagName("conferences");
+		if (nodeList.getLength() != 1) {
+			return false;
+		}
+
+		// Check that value is not null or empty
+		String password = getValue((Element) doc.getElementsByTagName("conferences").item(0), "nom");
+		if (password == null || password.isEmpty()) {
+			return false;
+		}
+
+		return true;
+	}
+	
+	private static String getValue(Element ele, String tagName) {
+		String value = null;
+		NodeList nl = ele.getElementsByTagName(tagName);
+		if(nl != null && nl.getLength() > 0) {
+			Element el = (Element)nl.item(0);
+			value = el.getFirstChild().getNodeValue();
+		}
+
+		return value;
+	}
+
+	public static Participant getParticipantVOFromXML(String xml) {
+		Document doc = getDocument(xml);
+		if (doc != null) {
+			if(validateParticipantXML(doc)) {
+				// XML is valid, just get the values from the DOM
+				return new Participant(Long.valueOf(getValue((Element) doc.getElementsByTagName("participant").item(0), "id")),
+						getValue((Element) doc.getElementsByTagName("participant").item(0), "dateinscription"),
+						getValue((Element) doc.getElementsByTagName("participant").item(0), "salutation"),
+						getValue((Element) doc.getElementsByTagName("participant").item(0), "prenom"),
+						getValue((Element) doc.getElementsByTagName("participant").item(0), "nom"),
+						Character.valueOf(getValue((Element) doc.getElementsByTagName("participant").item(0), "sexe").charAt(0)),
+						getValue((Element) doc.getElementsByTagName("participant").item(0), "email"),
+						getValue((Element) doc.getElementsByTagName("participant").item(0), "presentationsoumise"),
+						getValue((Element) doc.getElementsByTagName("participant").item(0), "cvsoumis"),
+						getValue((Element) doc.getElementsByTagName("participant").item(0), "details"),
+						getValue((Element) doc.getElementsByTagName("participant").item(0), "roleparticipant"));
+				
+				
+			}
+		
+		}		
+
+		return null;
+	}
+}
