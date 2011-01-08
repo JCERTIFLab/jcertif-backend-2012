9bdecc4cd5c3c9f2b7264fd27ee5c46a50d88336
Stanislas Matayo <MatayoBweta@gmail.com>
2011-01-08 11:43:02 +0000

    [BO]finalisation de Organisateur

--- jcertif-bo/src/main/java/com/jcertif/bo/Organisateur.java
+++ jcertif-bo/src/main/java/com/jcertif/bo/Organisateur.java
@@ -9,10 +9,7 @@ import java.util.Iterator;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
@@ -21,37 +18,21 @@ import org.apache.commons.lang.builder.EqualsBuilder;
 import org.apache.commons.lang.builder.HashCodeBuilder;
 
 /**
- * @author Utilisateur
+ * @author Douneg
  * 
  */
 @Entity
-public class Organisateur {
+public class Organisateur extends Person{
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 
 
-	@Id
-	@GeneratedValue
-	private Long id;
-
-	
 	/**
 	 * 
 	 */
-	@Column
-	private String prenom;
-	@Column
-	private String nom;
-	@Column
-	private Character sexe;
-	@Column
-	private String email;
-	@Column
-	private String telephone;
-	@Column
-	private String details;
+	
 	
 	@ManyToMany(cascade = CascadeType.ALL)
 	@JoinTable(name = "organisateur_conference", joinColumns = @JoinColumn(name = "organisateur_id"), inverseJoinColumns = @JoinColumn(name = "conference_id"))
@@ -68,81 +49,20 @@ public class Organisateur {
 	/**
 	 * Constructor
 	 */
-	public Organisateur(Long id, String prenom, String nom, Character sexe,
+	public Organisateur(Long id, String prenom, String nom, Character sexe_MF,
 			String email, String telephone, String details,
 			Set<Conference> conferences) {
 		super();
 		this.id = id;
 		this.prenom = prenom;
 		this.nom = nom;
-		this.sexe = sexe;
+		this.sexe_MF = sexe_MF;
 		this.email = email;
 		this.telephone = telephone;
 		this.details = details;
 		this.conferences = conferences;
 	}
 
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getPrenom() {
-		return prenom;
-	}
-
-	public void setPrenom(String prenom) {
-		this.prenom = prenom;
-	}
-
-	public String getNom() {
-		return nom;
-	}
-
-	public void setNom(String nom) {
-		this.nom = nom;
-	}
-	/**
-	 * @return the sexeMFTODO
-	 */
-	public Character getSexeMF() {
-		return sexe;
-	}
-
-	/**
-	 * @param sexeMF the sexeMF to setTODOsexeMF
-	 */
-	public void setSexeMF(Character sexeMF) {
-		this.sexe = sexeMF;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getTelephone() {
-		return telephone;
-	}
-
-	public void setTelephone(String telephone) {
-		this.telephone = telephone;
-	}
-
-	public String getDetails() {
-		return details;
-	}
-
-	public void setDetails(String details) {
-		this.details = details;
-	}
 
 	
 	public Set<Conference> getConferences() {
@@ -159,7 +79,7 @@ public class Organisateur {
 		xml.append("<id>").append(id).append("</id>");
 		xml.append("<prenom>").append(prenom).append("</prenom>");
 		xml.append("<nom>").append(nom).append("</nom>");
-		xml.append("<sexe>").append(sexe).append("</sexe>");
+		xml.append("<sexe>").append(sexe_MF).append("</sexe>");
 		xml.append("<email>").append(email).append("</email>");
 		xml.append("<telephone>").append(telephone).append("</telephone>");
 		xml.append("<details>").append(details).append("</details>");
@@ -185,7 +105,7 @@ public class Organisateur {
 		json.append("{\"conference\":{\"id\":\"").append(id)
 		.append("\", \"prenom\":\"").append(prenom)
 		.append("\", \"nom\":\"").append(nom)
-		.append("\", \"sexe\":\"").append(sexe)
+		.append("\", \"sexe\":\"").append(sexe_MF)
 		.append("\", \"email\":\"").append(email)
 		.append("\", \"telephone\":\"").append(telephone)
 		.append("\", \"details\":\"").append(details)
