22ceb244ea51c27384ab4cf2056321e25aac1e52
Rossi Oddet <rossi.oddet@gmail.com>
2011-01-08 18:09:03 +0000

    [BO] Auteur suppression d'import inutile

--- jcertif-bo/src/main/java/com/jcertif/bo/Auteur.java
+++ jcertif-bo/src/main/java/com/jcertif/bo/Auteur.java
@@ -9,10 +9,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
