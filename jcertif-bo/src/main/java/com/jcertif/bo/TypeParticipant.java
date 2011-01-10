1a03d29232306ca2a3d48aea4a16201f6ecb327f
Rossi Oddet <rossi.oddet@gmail.com>
2011-01-10 17:49:06 +0000

    [BO] Création BO TypeParticipant

---
+++ jcertif-bo/src/main/java/com/jcertif/bo/TypeParticipant.java
@@ -0,0 +1,33 @@
+package com.jcertif.bo;
+
+import javax.persistence.Entity;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * @author rossi.oddet
+ * 
+ */
+@Entity
+@XmlRootElement
+public class TypeParticipant extends CodeDescription {
+	
+	/**
+	 * Un constructeur.
+	 */
+	public TypeParticipant() {
+		super();
+	}
+
+	/**
+	 * Un constructeur.
+	 * 
+	 * @param code
+	 *            un code
+	 * @param desc
+	 *            une description
+	 */
+	public TypeParticipant(String code, String desc) {
+		this.setCode(code);
+		this.setDescription(desc);
+	}
+}
