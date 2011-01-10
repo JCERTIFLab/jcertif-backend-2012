d9ce01900d7fb9319de0d3484fa17b2a3c4bb1db
Rossi Oddet <rossi.oddet@gmail.com>
2011-01-10 17:48:38 +0000

    [BO] Création BO TypeEvenement

---
+++ jcertif-bo/src/test/java/com/jcertif/bo/TypeEvenementTest.java
@@ -0,0 +1,43 @@
+package com.jcertif.bo;
+
+import org.junit.Test;
+
+import com.evasion.framework.test.AccessorTesterImpl;
+import com.evasion.framework.test.EqualsTester;
+
+/**
+ * @author rossi.oddet
+ *
+ */
+public class TypeEvenementTest {
+
+	/**
+	 * Test de tous les accesseurs du Bo {@link TypeEvenement}.
+	 */
+	@Test
+	public void testAllAccessors() {
+		final TypeEvenement typeEvent = new TypeEvenement();
+		final AccessorTesterImpl accessorTester = new AccessorTesterImpl(typeEvent);
+		accessorTester.testAllAccessors();
+	}
+
+	/**
+	 * Test des méhodes Equals et Hashcode du Bo {@link TypeEvenement}.
+	 */
+	@Test
+	public void testtestEqualsAndHashCode() {
+		final TypeEvenement type = new TypeEvenement(
+				"type 1", "desc 1");
+		final TypeEvenement equivalentTypeEvenement1 = new TypeEvenement(
+				"type 1", "desc 1");
+		final TypeEvenement differentTypeEvenement1 = new TypeEvenement(
+				"type 3", "desc 1");
+		final TypeEvenement deriveTypeEvenement1 = new TypeEvenement(
+				"type 1", "desc 1") {
+		};
+		final EqualsTester vEqualTester = new EqualsTester();
+		vEqualTester.testEqualsAndHashCode(type,
+				equivalentTypeEvenement1, differentTypeEvenement1,
+				deriveTypeEvenement1);
+	}
+}
