e187ad89f0ed8c8dc6d4bf779bddafedd1545b2a
Rossi Oddet <rossi.oddet@gmail.com>
2011-01-12 23:00:32 +0000

    [BO] Suppression des caractères "_" dans les noms des champs du BO Evenement

--- jcertif-bo/src/main/java/com/jcertif/bo/Evenement.java
+++ jcertif-bo/src/main/java/com/jcertif/bo/Evenement.java
@@ -7,11 +7,9 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.Temporal;
 import javax.xml.bind.annotation.XmlRootElement;
 
 import org.apache.commons.lang.builder.EqualsBuilder;
