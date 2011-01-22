b24b637ab7125d5b70a20abb97c35c8771a1276a
Bernard Adanlessossi <adanlessossi@gmail.com>
2011-01-22 23:07:20 +0000

    Updated participant facade and type participant facade to reflect abstractfacade

--- jcertif-facade/src/main/java/com/jcertif/facade/participant/ParticipantFacade.java
+++ jcertif-facade/src/main/java/com/jcertif/facade/participant/ParticipantFacade.java
@@ -5,14 +5,8 @@ package com.jcertif.facade.participant;
 
 import java.util.List;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -48,9 +42,7 @@ public class ParticipantFacade extends AbstractFacade<ParticipantService, Partic
 	 *            the participant
 	 * @return the newly created {@link Participant}
 	 */
-	@PUT
 	@Path("/participant")
-	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 	public Participant createParticipant(Participant participant) {
 
 		LOG.debug("Creating a new Participant with Name: {}",
@@ -58,45 +50,37 @@ public class ParticipantFacade extends AbstractFacade<ParticipantService, Partic
 		return participantService.save(participant);
 	}
 
-	@GET
-	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	@Path("/participant/{id}")
 	/**
 	 * Searches for a participant by its key.
 	 */
+	@Path("/participant/{id}")
 	public Participant getParticipant(@PathParam("id") Long key) {
 		LOG.debug("Retrieving the participant with key: {}", key);
 		return participantService.findById(key);
 	}
 
-	@GET
-	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	@Path("/allparticipants")
 	/**
 	 * Retrieves the list of all participants.
 	 */
+	@Path("/allparticipants")
 	public List<Participant> findAllParticipants() {
 		LOG.debug("Retrieving the list of all participants");
 		return participantService.findAll();
 	}
-
-	@PUT
-	@Path("/updateparticipant")
+	
 	/**
 	 * Updates the given participant.
 	 */
-	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@Path("/updateparticipant")
 	public Participant updateParticipant(Participant participant) {
 		LOG.debug("Updating the participant with key: {}", participant.getId());
 		return participantService.update(participant);
 	}
 
-	@DELETE
-	@Path("/deleteparticipant")
 	/**
 	 * Deletes the participant.
 	 */
+	@Path("/deleteparticipant")
 	public void deleteParticipant(Participant participant) {
 		LOG.debug("Deleting the participant with key: {}", participant.getId());
 		participantService.remove(participant);
--- jcertif-facade/src/main/java/com/jcertif/facade/participant/TypeParticipantFacade.java
+++ jcertif-facade/src/main/java/com/jcertif/facade/participant/TypeParticipantFacade.java
@@ -5,13 +5,7 @@ package com.jcertif.facade.participant;
 
 import java.util.List;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.GET;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -39,64 +33,57 @@ private static final Logger LOG = LoggerFactory.getLogger(ParticipantFacade.clas
 	@Autowired
 	private TypeParticipantService service;
 	
-	@PUT
-	@Path("/typeparticipant/{id}")
-	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	
 	/**
 	 * Creates a new {@link TypeParticipant}.
 	 * @param type the new type participant
 	 * @return the type if created
 	 */
+	@Path("/typeparticipant")
 	public TypeParticipant createTypeParticipant(TypeParticipant type){
 		LOG.debug("Creating the type participant with key: {}", type.getId());
 		return service.save(type);
 	}
 	
-	@GET
-	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	@Path("/typeparticipant/{id}")
+	
 	/**
 	 * Gets a type participant by its id.
 	 * @param key the type id
 	 * @return the type
 	 */
+	@Path("/typeparticipant/{id}")
 	public TypeParticipant getTypeParticipantById(Long key){
 		LOG.debug("Finding the type participant with key: {}", key);
 		return service.findById(key);
 	}
 	
-	@GET
-	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	@Path("/alltypesparticipant")
+	
 	/**
 	 * Find all types participants.
 	 * @return a list of types
 	 */
+	@Path("/alltypesparticipant")
 	public List<TypeParticipant> findAllTypeParticipant(){
 		LOG.debug("Finding all type participant ");
 		return service.findAll();
 	}
 	
-	@PUT
-	@Path("/updatetypeparticipant")
 	/**
 	 * Updates the given type participant.
 	 * @param the type to update
 	 * @return the type if successfully updated
 	 */
-	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@Path("/updatetypeparticipant")
 	public TypeParticipant update(TypeParticipant type){
 		LOG.debug("Updating the type participant with key: {}", type.getId());
 		return service.update(type);
 	}
 	
-	@DELETE
-	@Path("/deletetypeparticipant")
 	/**
 	 * Deletes the given type participant from the database.
 	 * @param type the type to delete
 	 */
+	@Path("/deletetypeparticipant")
 	public TypeParticipant delete(TypeParticipant type){
 		LOG.debug("Deleting the type participant with key: {}", type.getId());
 		service.remove(type);
