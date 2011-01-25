1c9672b13605b9cf697e960ad8aa1de9dd7632f6
Rossi Oddet <rossi.oddet@gmail.com>
2011-01-25 20:31:03 +0000

    [FACADE/SERVICE] Création service/facade pour CeduleSalle

---
+++ jcertif-services/src/main/java/com/jcertif/service/api/cedule/CeduleSalleService.java
@@ -0,0 +1,14 @@
+package com.jcertif.service.api.cedule;
+
+import com.jcertif.bo.cedule.CeduleSalle;
+import com.jcertif.dao.api.cedule.CeduleSalleDAO;
+import com.jcertif.service.GenericService;
+
+/**
+ * @author rossi.oddet
+ * 
+ */
+public interface CeduleSalleService extends
+		GenericService<CeduleSalle, Long, CeduleSalleDAO> {
+
+}
---
+++ jcertif-services/src/main/java/com/jcertif/service/impl/cedule/CeduleSalleServiceImpl.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.jcertif.service.impl.cedule;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jcertif.bo.cedule.CeduleSalle;
+import com.jcertif.dao.api.cedule.CeduleSalleDAO;
+import com.jcertif.service.AbstractService;
+import com.jcertif.service.api.cedule.CeduleSalleService;
+
+/**
+ * @author rossi.oddet
+ * 
+ */
+@Service
+public class CeduleSalleServiceImpl extends
+		AbstractService<CeduleSalle, Long, CeduleSalleDAO> implements
+		CeduleSalleService {
+	
+	@Autowired
+	private CeduleSalleDAO ceduleSalleDAO;
+
+
+
+	@Override
+	public CeduleSalleDAO getDAO() {
+		return ceduleSalleDAO;
+	}
+
+	@Override
+	public void setDAO(CeduleSalleDAO dao) {
+		this.ceduleSalleDAO = dao;
+	}
+
+
+}
