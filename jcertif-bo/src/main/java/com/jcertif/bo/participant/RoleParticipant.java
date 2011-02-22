7316c1ab40b4c6d9231769ef4561905837096553
Stanislas Matayo <MatayoBweta@gmail.com>
2011-02-22 23:23:40 +0000

    [BO]Mise a jour de Role participant en lui donnant comme super classe CodeDescription

--- jcertif-bo/src/main/java/com/jcertif/bo/participant/RoleParticipant.java
+++ jcertif-bo/src/main/java/com/jcertif/bo/participant/RoleParticipant.java
@@ -3,157 +3,37 @@
  * roland.amour@chrisbel.net
  */
 package com.jcertif.bo.participant;
-
-import java.util.HashSet;
-import java.util.Set;
-
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 import javax.xml.bind.annotation.XmlRootElement;
 
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
 
-import com.jcertif.bo.AbstractBO;
+import com.jcertif.bo.CodeDescription;
 
 /**
  * @author chrisbel
  */
 @Entity
 @XmlRootElement
-public class RoleParticipant extends AbstractBO {
+public class RoleParticipant extends CodeDescription {
 
-	private static final long serialVersionUID = 1L;
-
-	/**
-	 * Identifiant du mot cl�.
-	 */
-	@Id
-	@GeneratedValue
-	private Long id;
-	
-	@Column
-	private String codeRole;
-	@Column
-	private String description;
-	/**
-	 * Mot cl� de la pr�sentation.
-	 */
-	@OneToMany
-	@JoinColumn(name ="role_participant_id")
-	private Set<Participant> participants=new HashSet<Participant>();
-	
-	
-	/**
-	 * Constructor
-	 */
-	public RoleParticipant() {
-		// TODO Auto-generated constructor stub
-	}
+    private static final long serialVersionUID = 1L;
 
 
-	/**
-	 * Constructor
-	 */
-	public RoleParticipant(Long id, String codeRole, String description) {
-		super();
-		this.id = id;
-		this.codeRole = codeRole;
-		this.description = description;
-	}
+    /**
+     * Constructor
+     */
+    public RoleParticipant() {
+        // TODO Auto-generated constructor stub
+    }
 
+    /**
+     * Constructor
+     */
+    public RoleParticipant(Long id, String code, String description) {
+        super();
+        this.setId(id);
+        this.setCode(code);
+        this.setDescription(description);
+    }
 
-	/**
-	 * @return the idTODO
-	 */
-	public Long getId() {
-		return id;
-	}
-
-
-	/**
-	 * @param id the id to setTODOid
-	 */
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-
-	/**
-	 * @return the codeRoleTODO
-	 */
-	public String getCodeRole() {
-		return codeRole;
-	}
-
-
-	/**
-	 * @param codeRole the codeRole to setTODOcodeRole
-	 */
-	public void setCodeRole(String codeRole) {
-		this.codeRole = codeRole;
-	}
-
-
-	/**
-	 * @return the descriptionTODO
-	 */
-	public String getDescription() {
-		return description;
-	}
-
-
-	/**
-	 * @param description the description to setTODOdescription
-	 */
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	/**
-	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
-	public int hashCode() {
-		return new HashCodeBuilder().append(id).append(codeRole)
-				.toHashCode();
-	}
-
-	/**
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(Object obj) {
-
-		if (this == obj) {
-			return true;
-		}
-
-		if (!(obj instanceof RoleParticipant)) {
-			return false;
-		}
-
-		final RoleParticipant other = (RoleParticipant) obj;
-
-		return new EqualsBuilder().append(id, other.getId()).append(codeRole, other.getCodeRole()).isEquals();
-	}
-
-
-	/**
-	 * @return the participantsTODO
-	 */
-	public Set<Participant> getParticipants() {
-		return participants;
-	}
-
-
-	/**
-	 * @param participants the participants to setTODOparticipants
-	 */
-	public void setParticipants(Set<Participant> participants) {
-		this.participants = participants;
-	}
 }
