608d879d4fb5aec072b8d7f904448203a1175a0d
Rossi Oddet <rossi.oddet@gmail.com>
2011-03-13 23:41:15 +0000

    [IHM] Amélioration du look du formulaire d'inscription par l'application d'un nouveau thème. Envoi du mail lors de la création d'un participant OK. Il reste à améliorer la gestion des erreurs et mettre tous les champs suivant la définition de Max et Aminata

--- jcertif-facade/src/main/java/com/jcertif/facade/participant/ParticipantFacade.java
+++ jcertif-facade/src/main/java/com/jcertif/facade/participant/ParticipantFacade.java
@@ -3,7 +3,11 @@
  */
 package com.jcertif.facade.participant;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +16,7 @@ import com.jcertif.bo.participant.Participant;
 import com.jcertif.dao.api.participant.ParticipantDAO;
 import com.jcertif.facade.AbstractFacade;
 import com.jcertif.service.api.participant.ParticipantService;
+import com.jcertif.service.mail.CSenderService;
 import com.sun.jersey.api.spring.Autowire;
 
 /**
@@ -24,6 +29,9 @@ import com.sun.jersey.api.spring.Autowire;
 @Autowire
 public class ParticipantFacade extends
         AbstractFacade<ParticipantService, Participant, Long> {
+	
+	@Autowired
+    private CSenderService cSenderService;
 
 	@Autowired
 	private ParticipantService participantService;
@@ -37,4 +45,16 @@ public class ParticipantFacade extends
 	public void setService(ParticipantService service) {
 		this.participantService = service;
 	}
+
+	@POST
+	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@Path(CREATE_SUFFIX)
+	public Participant create(Participant entity) {
+		Participant part = super.create(entity);
+		cSenderService.sendAddParticipantConfirmation(entity);
+		return part;
+	}
+	
+	
 }
