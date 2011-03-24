808b8db4da87621a54ef11ddef45e0e2e8bcb305
Rossi Oddet <rossi.oddet@gmail.com>
2011-03-24 22:59:25 +0000

    [IHM] Sujet Servicee

---
+++ jcertif-services/src/main/java/com/jcertif/service/api/presentation/SujetService.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.jcertif.service.api.presentation;
+
+import com.jcertif.bo.presentation.Sujet;
+import com.jcertif.dao.api.presentation.SujetDAO;
+import com.jcertif.service.GenericService;
+
+/**
+ * @author rossi
+ * 
+ */
+public interface SujetService extends GenericService<Sujet, Long, SujetDAO> {
+
+}
