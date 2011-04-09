1f0b02c76931e1f4877d78b664fa6841d7655fff
Rossi Oddet <rossi.oddet@gmail.com>
2011-04-09 14:10:13 +0000

    [IHM] Ajout du fichier de constantes

---
+++ jcertif-presentation/src/main/java/com/jcertif/presentation/ui/util/UIConst.java
@@ -0,0 +1,15 @@
+package com.jcertif.presentation.ui.util;
+
+/**
+ * constants for ui layer.
+ * 
+ * @author rossi
+ * 
+ */
+public interface UIConst {
+
+	String URL_SPEAKER_IMG = "./images/speakers/";
+
+	String CALENDAR_DETAIL_LAYOUT = "details_event_layout";
+
+}
