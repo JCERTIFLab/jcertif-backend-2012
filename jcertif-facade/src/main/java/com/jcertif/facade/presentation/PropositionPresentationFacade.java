40e32d839f277bba55ea0306a18e62f96c0e8041
Rossi Oddet <rossi.oddet@gmail.com>
2011-04-11 18:16:48 +0000

    [IHM] Suppression du mode dégradé pour proposition presentation

--- jcertif-facade/src/main/java/com/jcertif/facade/presentation/PropositionPresentationFacade.java
+++ jcertif-facade/src/main/java/com/jcertif/facade/presentation/PropositionPresentationFacade.java
@@ -3,11 +3,14 @@
  */
 package com.jcertif.facade.presentation;
 
+import java.util.List;
 import java.util.Set;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -47,6 +50,31 @@ public class PropositionPresentationFacade extends
 		this.service = service;
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.jcertif.facade.AbstractFacade#findAll()
+	 */
+	@Override
+	public List<PropositionPresentation> findAll() {
+		List<PropositionPresentation> props = super.findAll();
+		for (PropositionPresentation prop : props) {
+			removeCycle(prop);
+		}
+
+		return props;
+	}
+
+	@GET
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@Path(FINDBYID_SUFFIX)
+	@Override
+	public PropositionPresentation findById(@PathParam("id") Long id) {
+		PropositionPresentation prop = super.findById(id);
+		removeCycle(prop);
+		return prop;
+	}
+
 	@POST
 	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
@@ -54,12 +82,18 @@ public class PropositionPresentationFacade extends
 	@Override
 	public PropositionPresentation create(PropositionPresentation clientPropo) {
 		Set<Participant> participantSet = clientPropo.getParticipants();
-		// TODO résoudre le bug de la sauvegarde
-		clientPropo.setParticipants(null);
 		PropositionPresentation proposition = super.create(clientPropo);
+		proposition = service.findById(proposition.getId());
 		clientPropo.setParticipants(participantSet);
 		cSenderService.sendAddPropositionConfirmation(clientPropo);
+		removeCycle(proposition);
 		return proposition;
+	}
+
+	private void removeCycle(PropositionPresentation prop) {
+		for (Participant part : prop.getParticipants()) {
+			part.setPropositionPresentations(null);
+		}
 	}
 
 }
