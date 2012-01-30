35715e074c5bb7920ad67cf9c92f8c6836d11026
Rossi Oddet <rossi.oddet@gmail.com>
2012-01-30 17:53:46 +0000

    [FACADE] Ajout d'un service de récupération d'une conférence par id

--- jcertif-facade/src/main/java/com/jcertif/facade/model/conference/Conference.java
+++ jcertif-facade/src/main/java/com/jcertif/facade/model/conference/Conference.java
@@ -5,83 +5,85 @@
 package com.jcertif.facade.model.conference;
 
 import java.util.Calendar;
+
 import javax.xml.bind.annotation.XmlRootElement;
 
 /**
- *
+ * 
  * @author rossi.oddet
  */
- @XmlRootElement
+@XmlRootElement
 public class Conference {
 
-    private Long id;
-    private String nom;
-    private Calendar dateDebut;
-    private Calendar dateFin;
-    private String website;
-    private String details;
+	private Long id;
+	private String nom;
+	private Calendar dateDebut;
+	private Calendar dateFin;
+	private String website;
+	private String details;
 
-    public Conference() {
-        super();
-    }
+	public Conference() {
+		super();
+	}
 
-    public Conference(com.jcertif.bo.conference.Conference conf) {
-        this.id = conf.getId();
-        this.nom = conf.getNom();
-        this.dateDebut = conf.getDateDebut();
-        this.dateFin = conf.getDateFin();
-        this.website = conf.getWebsite();
-        this.details = conf.getDetails();
-    }
+	public Conference(com.jcertif.bo.conference.Conference conf) {
+		if (conf != null) {
+			this.id = conf.getId();
+			this.nom = conf.getNom();
+			this.dateDebut = conf.getDateDebut();
+			this.dateFin = conf.getDateFin();
+			this.website = conf.getWebsite();
+			this.details = conf.getDetails();
+		}
 
-    public Calendar getDateDebut() {
-        return dateDebut;
-    }
+	}
 
-    public void setDateDebut(Calendar dateDebut) {
-        this.dateDebut = dateDebut;
-    }
+	public Calendar getDateDebut() {
+		return dateDebut;
+	}
 
-    public Calendar getDateFin() {
-        return dateFin;
-    }
+	public void setDateDebut(Calendar dateDebut) {
+		this.dateDebut = dateDebut;
+	}
 
-    public void setDateFin(Calendar dateFin) {
-        this.dateFin = dateFin;
-    }
+	public Calendar getDateFin() {
+		return dateFin;
+	}
 
-    public String getDetails() {
-        return details;
-    }
+	public void setDateFin(Calendar dateFin) {
+		this.dateFin = dateFin;
+	}
 
-    public void setDetails(String details) {
-        this.details = details;
-    }
+	public String getDetails() {
+		return details;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public void setDetails(String details) {
+		this.details = details;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public String getNom() {
-        return nom;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
+	public String getNom() {
+		return nom;
+	}
 
-    public String getWebsite() {
-        return website;
-    }
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
 
-    public void setWebsite(String website) {
-        this.website = website;
-    }
+	public String getWebsite() {
+		return website;
+	}
 
-    
-    
+	public void setWebsite(String website) {
+		this.website = website;
+	}
+
 }
--- jcertif-facade/src/main/java/com/jcertif/facade/rest/conference/ConferenceFacade.java
+++ jcertif-facade/src/main/java/com/jcertif/facade/rest/conference/ConferenceFacade.java
@@ -1,22 +1,23 @@
 package com.jcertif.facade.rest.conference;
 
-
-import com.jcertif.facade.Facade;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.jcertif.facade.Facade;
 import com.jcertif.facade.model.conference.Conference;
 import com.jcertif.service.api.conference.ConferenceService;
 import com.sun.jersey.api.spring.Autowire;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
-import javax.ws.rs.GET;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 /**
  * Facade pour service participant.
@@ -29,19 +30,28 @@ import javax.ws.rs.core.MediaType;
 @Autowire
 public class ConferenceFacade extends Facade {
 
-    @Autowired
-    private ConferenceService service;
+	@Autowired
+	private ConferenceService service;
 
-    @GET
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    @Path(FINDALL_SUFFIX)
-    public List<Conference> findAll() {
-        List<Conference> confList = new ArrayList<Conference>();
-        Set<com.jcertif.bo.conference.Conference> confSet = new HashSet<com.jcertif.bo.conference.Conference>(service.findAll());
-        
-        for(com.jcertif.bo.conference.Conference conf : confSet){
-            confList.add(new Conference(conf));
-        }
-        return confList;
-    }
+	@GET
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@Path(FINDALL_SUFFIX)
+	public List<Conference> findAll() {
+		List<Conference> confList = new ArrayList<Conference>();
+		Set<com.jcertif.bo.conference.Conference> confSet = new HashSet<com.jcertif.bo.conference.Conference>(
+				service.findAll());
+
+		for (com.jcertif.bo.conference.Conference conf : confSet) {
+			confList.add(new Conference(conf));
+		}
+		return confList;
+	}
+
+	@GET
+	@Produces(value = { MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	@Path("/{id}")
+	public Conference findById(@PathParam("id") Long id) {
+		Conference conf = new Conference(service.findById(id));
+		return conf;
+	}
 }
