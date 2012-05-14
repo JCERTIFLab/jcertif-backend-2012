df49a61805b402a8ac438a34eb1b099a737b273a
Rossi Oddet <rossi.oddet@gmail.com>
2012-05-14 20:07:59 +0000

    [SERVICES] Le service de récupération des speakers ne revoient désormais uniquement les speakers qui ont au moins une proposition au statut validé.

--- jcertif-services/src/main/java/com/jcertif/service/impl/participant/SpeakerServiceImpl.java
+++ jcertif-services/src/main/java/com/jcertif/service/impl/participant/SpeakerServiceImpl.java
@@ -6,10 +6,12 @@ package com.jcertif.service.impl.participant;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +20,7 @@ import org.springframework.stereotype.Service;
 import com.jcertif.bo.conference.Conference;
 import com.jcertif.bo.participant.Participant;
 import com.jcertif.bo.participant.RoleParticipant;
+import com.jcertif.bo.presentation.PropositionPresentation;
 import com.jcertif.dao.api.conference.ConferenceDAO;
 import com.jcertif.dao.api.participant.ParticipantDAO;
 import com.jcertif.dao.api.participant.RoleParticipantDAO;
@@ -30,7 +33,8 @@ import com.jcertif.service.api.participant.SpeakerService;
 @Service
 public class SpeakerServiceImpl implements SpeakerService {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(ParticipantServiceImpl.class);
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(ParticipantServiceImpl.class);
 
 	@Autowired
 	private ParticipantDAO participantDAO;
@@ -41,8 +45,8 @@ public class SpeakerServiceImpl implements SpeakerService {
 
 	@Override
 	public Set<Participant> findAll(Long idConference) {
-		List<RoleParticipant> roleSpeakerList = roleParticipantDAO.findByProperty("code",
-				RoleParticipant.CODE_SPEAKER);
+		List<RoleParticipant> roleSpeakerList = roleParticipantDAO
+				.findByProperty("code", RoleParticipant.CODE_SPEAKER);
 
 		Conference conf = conferenceDAO.findById(idConference);
 
@@ -54,7 +58,30 @@ public class SpeakerServiceImpl implements SpeakerService {
 		criterias.put("roleparticipant", roleSpeakerList.iterator().next());
 		criterias.put("conference", conf);
 
-		return new HashSet<Participant>(participantDAO.findByProperties(criterias));
+		Set<Participant> participants = new HashSet<Participant>(
+				participantDAO.findByProperties(criterias));
+
+		Iterator<Participant> participantIt = participants.iterator();
+		
+		boolean isValidSpeaker = false;
+
+		while (participantIt.hasNext()) {
+			Participant participant = participantIt.next();
+			isValidSpeaker = false;
+			if (participant.getPropositionPresentations() != null && !participant.getPropositionPresentations().isEmpty()) {
+				for (PropositionPresentation suggest : participant.getPropositionPresentations()) {
+					if("A".equalsIgnoreCase(suggest.getStatutApprobation().getCodeStatut())){
+						isValidSpeaker = true;
+						break;
+					}
+				}
+			}
+			if(!isValidSpeaker) {
+				participantIt.remove();
+			}
+		}
+
+		return participants;
 	}
 
 }
